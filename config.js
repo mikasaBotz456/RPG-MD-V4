9b7631f4ac21cefc6a5db3857bb6e27fa9355b96
mikasaBotz456 <108715025+mikasaBotz456@users.noreply.github.com>
2022-11-03 14:32:53 +0700

    update config.js

--- config.js
+++ config.js
@@ -372,7 +372,7 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 /*============== STICKER WM ==============*/
 global.stickpack = 'ᴍᴀᴅᴇ ʙʏ'
-global.stickauth = `King Of Bear`
+global.stickauth = `Mikasa`
 
 global.multiplier = 38 // The higher, The harder levelup
 
